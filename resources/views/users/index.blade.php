873a1f490e355102e15e5bb5755940e481bbbd60
YashAjmer <162540674+YashAjmer@users.noreply.github.com>
2024-08-16 12:22:46 +0530

    Task Management System assignment Complete

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -45,7 +45,6 @@
 
 
 
-                    <a class="btn btn-info" href="{{ route('users.show', $user->id) }}">Show</a>
                     @can('user-edit')
                         <a class="btn btn-primary" href="{{ route('users.edit', $user->id) }}">Edit</a>
                     @endcan
